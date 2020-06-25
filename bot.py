0426ee66647cd4b34420b40bffa63018e580b1ec
0Zeta <0Zeta@protonmail.com>
2020-06-25 18:37:08 +0200

    Make agent spawn a shipyard immediately

--- bot.py
+++ bot.py
@@ -7,11 +7,21 @@ logging.basicConfig(level=logging.WARNING)
 env = make("halite", debug=True)
 DIRECTIONS = ['NORTH', 'EAST', 'SOUTH', 'WEST']
 
+CONFIG = None
+SIZE = 21
+
+ME = None
+
 PLANNED_MOVES = list()  # a list of positions where our ships will be in the next step
 
 
 def handle_special_steps(board: Board):
-    pass
+    step = board.step
+    if step == 0:
+        # Immediately spawn a shipyard
+        ME.ships[0].next_action = ShipAction.CONVERT
+        return True
+    return False
 
 
 def handle_halite_deposits(board: Board):
@@ -29,4 +39,16 @@ def move_ships(board: Board):
 @board_agent
 def agent(board: Board):
     obs = board.observation
-    logging.debug("Begin step " + str(obs.step))
+    global CONFIG
+    if CONFIG is None:
+        CONFIG = board.configuration
+        global SIZE
+        SIZE = CONFIG.size
+
+    global ME
+    ME = board.current_player
+
+    logging.debug("Begin step " + str(board.step))
+
+    if handle_special_steps(board):
+        return  # don't execute the functions below
