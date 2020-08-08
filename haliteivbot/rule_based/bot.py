07675d263577cb9326101e8956cd653a6765f556
0Zeta <0Zeta@protonmail.com>
2020-08-08 02:44:52 +0200

    Fixed a bug that caused the bot not to build a shipyard when there is none

--- haliteivbot/rule_based/bot.py
+++ haliteivbot/rule_based/bot.py
@@ -13,60 +13,60 @@ env = make("halite", debug=True)
 
 PARAMETERS = {
     'cell_score_enemy_halite': 0.4715114974967425,
-    'cell_score_neighbour_discount': 0.6163060180776797,
+    'cell_score_neighbour_discount': 0.6103114250591687,
     'cell_score_ship_halite': 0.0006146935882821743,
-    'conflict_map_alpha': 1.5953629094011479,
+    'conflict_map_alpha': 1.5582849784278563,
     'conflict_map_sigma': 0.7972236445783817,
     'conflict_map_zeta': 0.8625720662620574,
     'convert_when_attacked_threshold': 600,
     'disable_hunting_till': 66,
-    'dominance_map_halite_clip': 300,
+    'dominance_map_halite_clip': 350,
     'dominance_map_medium_radius': 5,
-    'dominance_map_medium_sigma': 0.1451052789136152,
+    'dominance_map_medium_sigma': 0.01,
     'dominance_map_small_radius': 3,
-    'dominance_map_small_sigma': 0.5568142087135179,
+    'dominance_map_small_sigma': 0.6398185917717716,
     'end_return_extra_moves': 6,
     'end_start': 381,
     'ending_halite_threshold': 27,
-    'hunting_halite_threshold': 6,
+    'hunting_halite_threshold': 13,
     'hunting_min_ships': 13,
-    'hunting_score_alpha': 1.0401304403087919,
+    'hunting_score_alpha': 1.1279514150456798,
     'hunting_score_beta': 2.5942517199955524,
-    'hunting_score_delta': 0.5142337849582957,
-    'hunting_score_gamma': 0.99,
-    'hunting_threshold': 5.116440638204147,
+    'hunting_score_delta': 0.5316893166228572,
+    'hunting_score_gamma': 0.948335898856567,
+    'hunting_threshold': 5.923173510655287,
     'map_blur_gamma': 0.8013766949565679,
-    'map_blur_sigma': 0.6548270958946582,
-    'max_halite_attack_shipyard': 194,
+    'map_blur_sigma': 0.693995126086896,
+    'max_halite_attack_shipyard': 220,
     'max_hunting_ships_per_direction': 2,
     'max_ship_advantage': 6,
     'max_shipyard_distance': 11,
     'max_shipyards': 2,
-    'min_mining_halite': 37,
-    'min_ships': 26,
+    'min_mining_halite': 43,
+    'min_ships': 25,
     'min_shipyard_distance': 3,
     'mining_score_alpha': 0.99,
-    'mining_score_beta': 0.9288702287635913,
-    'mining_score_dominance_clip': 3.85172304343892,
-    'mining_score_dominance_norm': 0.6,
-    'mining_score_gamma': 0.9887772786493401,
+    'mining_score_beta': 0.9677377968883031,
+    'mining_score_dominance_clip': 3.7917976330210568,
+    'mining_score_dominance_norm': 0.5259866301386806,
+    'mining_score_gamma': 0.99,
     'move_preference_base': 109,
     'move_preference_hunting': 113,
     'move_preference_longest_axis': 12,
     'move_preference_mining': 130,
-    'move_preference_return': 116,
+    'move_preference_return': 115,
     'move_preference_stay_on_shipyard': -112,
-    'return_halite': 1850,
+    'return_halite': 2509,
     'ship_spawn_threshold': 0.8698654657611027,
     'ships_shipyards_threshold': 0.05,
     'shipyard_abandon_dominance': -3.299048786606774,
     'shipyard_conversion_threshold': 2.589602412992243,
     'shipyard_guarding_attack_probability': 0.7405296328744754,
-    'shipyard_guarding_min_dominance': 5.943371947953334,
-    'shipyard_min_dominance': 6.956797024987677,
+    'shipyard_guarding_min_dominance': 6.869301077098315,
+    'shipyard_min_dominance': 6.976792091761268,
     'shipyard_start': 48,
-    'shipyard_stop': 286,
-    'spawn_min_dominance': 5.290290410672599,
+    'shipyard_stop': 271,
+    'spawn_min_dominance': 5.01915987533356,
     'spawn_till': 312
 }
 
@@ -289,7 +289,7 @@ class HaliteBot(object):
         self.hunting_ships.clear()
 
         if self.shipyard_count == 0:
-            ship = max(self.me.ships, key=lambda ship: ship.halite)
+            ship = max(self.me.ships, key=lambda ship: ship.halite)  # TODO: choose the ship with the safest position
             if ship.halite + self.halite >= self.config.convert_cost:
                 self.convert_to_shipyard(ship)
                 self.ship_types[ship.id] = ShipType.CONVERTING
@@ -690,7 +690,7 @@ class HaliteBot(object):
         # ship.next_action = ShipAction.CONVERT
         self.ship_types[ship.id] = ShipType.CONVERTING
         self.ship_position_preferences[self.ship_to_index[ship],
-        len(self.position_to_index):-1] = 9999999  # TODO: fix the amount of available shipyard conversions
+        len(self.position_to_index):] = 9999999  # TODO: fix the amount of available shipyard conversions
         self.halite += ship.halite
         self.halite -= self.config.convert_cost
         self.ship_count -= 1
--- haliteivbot/rule_based/evolutionary_optimization.py
+++ haliteivbot/rule_based/evolutionary_optimization.py
@@ -14,7 +14,7 @@ SELECTION_CAP = 5  # take the fittest five genomes of a generation
 IGNORE_SELECTION_PROBABILITY = 0.1  # the probability to let another genome survive
 NB_PARENTS = 3
 
-POOL_NAME = ""
+POOL_NAME = "2020-08-08 01-48"
 
 hyperparameters = {
     'cell_score_enemy_halite': ('float', (0.15, 0.5)),
