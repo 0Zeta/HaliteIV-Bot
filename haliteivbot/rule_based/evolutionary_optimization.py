5c719cceab3073b1ec1bd40ebfe5366f88549bb0
0Zeta <0Zeta@protonmail.com>
2020-08-14 21:45:52 +0200

    Don't spawn ships to defend a shipyard or build shipyards after the start of the ending phase

--- haliteivbot/rule_based/evolutionary_optimization.py
+++ haliteivbot/rule_based/evolutionary_optimization.py
@@ -11,7 +11,7 @@ SELECTION_CAP = 5  # take the fittest five genomes of a generation
 IGNORE_SELECTION_PROBABILITY = 0.1  # the probability to let another genome survive
 NB_PARENTS = 3
 
-POOL_NAME = "2020-08-14 14-27"
+POOL_NAME = "2020-08-14 19-40"
 
 hyperparameters = {
     'cargo_map_halite_norm': ('int', (50, 500)),
